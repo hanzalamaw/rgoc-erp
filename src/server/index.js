e00b294bf97ff076ea9c302906cad025b5f81799
Hanzala M A Wahab <hanzalamawahab@gmail.com>
2025-06-25 20:10:14 +0500

    a

--- src/server/index.js
+++ src/server/index.js
@@ -20,10 +20,14 @@ app.use(cors());
 app.use(express.json());
 
 const db = await mysql.createConnection({
-  host: 'localhost',
+  /*host: 'localhost',
   user: 'root',
   password: '',
-  database: 'rgoc-erp' 
+  database: 'rgoc-erp'*/ 
+  host: 'webhouseinc.co',
+  user: 'webhngff_hanzalamaw',
+  password: 'Hanzala1234!',
+  database: 'webhngff_rgoc-erp'
 });
 
 // 🔐 Login route
@@ -246,4 +250,8 @@ app.post('/api/bookings/update-status', async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log('✅ Server running on http://localhost:5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
