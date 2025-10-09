474473a4d4f774255c395434880173ca48e54703
hanzalamaw <hanzalamawahab@gmail.com>
2025-10-09 20:42:19 +0500

    New Commit

--- src/components/loans.jsx
+++ src/components/loans.jsx
@@ -1,11 +1,16 @@
 import './loans.css';
 
 function Loans(){
+
+    const addNewLoan = () => {
+        alert("Add New Loan button clicked!");
+    }
+
     return (
     <> 
     <div className='loanHeader'>
         <h1>LOAN MANAGEMENT</h1>
-        <button className="addNewLoanBtn">Add New Loan</button>  
+        <button className="addNewLoanBtn" onClick={addNewLoan}>Add New Loan</button>
     </div>
     </>
     )
